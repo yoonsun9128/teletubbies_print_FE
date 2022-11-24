5fc07c16eb173857ca7c20dc2bfd72189dbfaffb
jangwon-seo <sjw887@naver.com>
2022-11-24 11:33:48 +0900

    [ADD] api.js, signup.html, login.html

---
+++ js/api.js
@@ -0,0 +1,80 @@
+// 전역 변수 //
+const backend_base_url = 'http://127.0.0.1:8000/'
+const frontend_base_url = 'http://127.0.0.1:5500/templates/'
+
+// # 검사에서 콘솔 잘 찍히는지 확인 나중에 삭제 //
+window.onload = () => {
+    console.log("로딩되었음") 
+}
+
+// # 회원가입 //
+async function handleSignin(){
+    const username = document.getElementById("username").value
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+    const passwordcheck = document.getElementById("passwordcheck").value
+    const address = document.getElementById("address").value
+    const phone_number = document.getElementById("phone_number").value
+    console.log(username, email, password, address, phone_number)
+
+    const response = await fetch(`${backend_base_url}user/signup/`, {
+        headers:{
+            'content-type':'application/json',
+        },
+        method:'POST',
+        body: JSON.stringify({
+            "username":username,
+            "email":email,
+            "phone_number":phone_number,
+            "address":address,
+            "password":password,
+            "passwordcheck":passwordcheck,
+            
+            
+        })
+    })
+
+    response_json = await response.json()
+    
+    if (response.status == 201) {
+        window.location.replace(`${frontend_base_url}login.html`)
+    } else {
+        alert("조건에 맞춰 입력해주세요.")
+    }
+}
+
+// # 로그인 //
+async function handleLogin(){
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+    
+    const response = await fetch('http://127.0.0.1:8000/users/api/token/', {
+        headers:{
+            'content-type':'application/json',
+        },
+        method:'POST',
+        body: JSON.stringify({
+            "email":email,
+            "password":password
+        })
+    })
+
+    const response_json = await response.json()
+
+    console.log(response_json)
+
+    localStorage.setItem("access", response_json.access);
+    localStorage.setItem("refresh", response_json.refresh);
+
+    
+    
+    const base64Url = response_json.access.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    localStorage.setItem("payload", jsonPayload); 
+
+
+}
