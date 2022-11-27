6ffd1e36dfa14a8a073e28e5d683f492a2c8ff21
nhkmi1001 <nhkmi1001@gmail.com>
2022-11-28 03:52:27 +0900

    [MOD] main.js html

--- js/imgtransfer.js
+++ js/imgtransfer.js
@@ -11,7 +11,7 @@ window.onload = () => {
 
 async function getFilter(){
     console.log(filterId)
-    const response = await fetch('http://127.0.0.1:8000/store/${filterId}/',{
+    const response = await fetch(`http://127.0.0.1:8000/store/${filterId}/`,{
         headers: {
             'Content-Type': 'application/json',
             'Authorization': 'Bearer' + localStorage.getItem("access")
