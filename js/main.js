2cbd2544bf12b9e3f86e4c5102c45bb2e956913b
nhkmi1001 <nhkmi1001@gmail.com>
2022-11-27 16:31:55 +0900

    [MOD] main.js / main.html 완료

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 // 전역 변수 //
 const backend_base_url = 'http://127.0.0.1:8000/'
 const frontend_base_url = 'http://127.0.0.1:5500/templates/'
@@ -23,17 +24,13 @@ window.onload = async function GotoOptionPage(){
             console.log(filter[i]['filter_image'])
 
             let temp_html = `
-            <li>
-                <div class="products">
                     <!-- 게시글 -->
-                    <div class="filter" id="${id}" onclick="location.href='${frontend_base_url}/templates/ordersetting.html?id=${filter_id}'">
-                        <img src="${filter_image}">
+                    <div class="filter" id="${id}" onclick="location.href='${frontend_base_url}imgupload.html?id=${id}'">
+                        <img src="${backend_base_url}${filter_image}">
                     </div>
-                </div>
-            </li>
+        
             `
+            $('#filter-box').append(temp_html)
         }
     })
-            $('#filter-box').append(temp_html)
-            console.log(filter)
 }
