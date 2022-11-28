d16e0ef5704e9dfcadab55784f95f63be026c27d
nhkmi1001 <nhkmi1001@gmail.com>
2022-11-28 10:13:20 +0900

    [MOD] ...

--- js/imgtransfer.js
+++ js/imgtransfer.js
@@ -5,6 +5,27 @@ const backend_base_url = 'http://127.0.0.1:8000/'
 const filterId = localStorage.getItem('filter_id')
 console.log(filterId)
 
+function setCookie(cookieName, cookieValue, cookieExpire, cookiePath, cookieDomain, cookieSecure){
+    var cookieText=escape(cookieName)+'='+escape(cookieValue);
+    cookieText+=(cookieExpire ? '; EXPIRES='+cookieExpire.toGMTString() : '');
+    cookieText+=(cookiePath ? '; PATH='+cookiePath : '');
+    cookieText+=(cookieDomain ? '; DOMAIN='+cookieDomain : '');
+    cookieText+=(cookieSecure ? '; SECURE' : '');
+    document.cookie=cookieText;
+}
+ 
+function getCookie(cookieName){
+    var cookieValue=null;
+    if(document.cookie){
+        var array=document.cookie.split((escape(cookieName)+'='));
+        if(array.length >= 2){
+            var arraySub=array[1].split(';');
+            cookieValue=unescape(arraySub[0]);
+        }
+    }
+    return cookieValue;
+}
+
 window.onload = () => {
     getFilter();
 }
@@ -13,19 +34,20 @@ async function getFilter(){
     const response = await fetch(`http://127.0.0.1:8000/store/${filterId}/`,{
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer' + localStorage.getItem("access")
+            'Authorization': 'Bearer ' + localStorage.getItem("access")
         },
         method:'GET',
     })
     data = await response.json()
     console.log(data)
+    
 
     let click_image = data['filter_image']
     console.log(data['filter_image'])
 
     let temp_html = `
     <div class="image-container">
-            <img style="width: 500px;" id="previewImage" src="${backend_base_url}${click_image}">
+            <img style="width: 500px;" src="${backend_base_url}${click_image}">
     </div>
     `
     $('#filterImage').append(temp_html)
@@ -44,16 +66,20 @@ const handleFiles = (e) => {
 }
 fileInput.addEventListener("change", handleFiles)
 
-
+// var headers = new Headers();
+// var csrftoken = getCookie('csrftoken');
+// headers.append('X-CSRFToken', csrftoken);
+// headers.append('Accept', 'application/json, text/plain, */*');
+// headers.append('Content-Type', 'application/x-www-form-urlencoded');
+// headers.append('Authorization', 'Bearer ' + localStorage.getItem("access"))
 
 async function transferML(){
-    const image=document.getElementById("InputImg").files[0]
+    const image=document.getElementById("InputImg").files
     console.log(image)
     const formData = new FormData();
     console.log("52",formData)
 
     formData.append('input_img', image);
-    console.log(formData.value)
 
     for (var pair of formData.entries()) {
         console.log(pair[0]+ ', ' + pair[1]);
@@ -62,16 +88,29 @@ async function transferML(){
     const response = await fetch (`http://127.0.0.1:8000/store/${filterId}/`, {
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer' + localStorage.getItem("access")
+            'Authorization': 'Bearer ' + localStorage.getItem("access"),
+            'Accept': 'application/json'
         },
         method:"POST",
-        body:formData
+        body: JSON.stringify(formData)
     })
+    response_json = await response.json()
 
     if (response.status ==200){
-        alert("사진 변환중")
+        console.log("불러오기", token)
         window.location.replace('option.html');
+
     }else{
-        alert(response.status)
+        alert("잘못된 로그인입니다.", response.status)
     }
 }
+
+function parseJwt(token) {
+    var base64Url = localStorage.getItem("access").split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(
+        function (c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+    return JSON.parse(jsonPayload);
+};
